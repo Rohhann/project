f586492f3fbd5f65e3e46d9612b70df5c83a18c9
Denis <the-unforgiven@mail.ru>
2020-01-06 20:26:08 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+'use strict';
+
+let a = 5;
+
+console.log(a);
+
+
+console.log(4/0);
+console.log('string'*9);
+
+
+let something;
+console.log(something);
+
+
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone);
+
+
+
+let arr = ['plun.png','orange.jpg','apple.bmp'];
+
+console.log(arr);
+
+alert("Hello World!");
+
+let answer = confirm("Are u here?");
+
+console.log(answer);
+
+
+let questions = +prompt("Вам есть 18 лет?", "Да");
+
+console.log(typeof(null));
+
+console.log("arr" + " - object");
+
+console.log(4 + +" - object");
+
+let incr = 10, decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = true, isClose = false;
+
+console.log(isChecked || isClose);
